6c1453042c541dddfc89cb0b5087f5c96e148449
NMcVayZag <Nickwmcvay15@gmail.com>
2022-04-21 08:19:36 -0700

    update

---
+++ Utils2.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+#def pull_helium_activity(helium_address,min_time):
+    #url = "https://api.helium.io/v1/hotspots/:address/activity"
+    #url += "?address="+ helium_address + "&min_time=" + "1593622131"
+    #response = requests.get(url=url)
+    #json_str = response.text        #takes response turning it to a string
+    #json_obj = json.loads(json_str)
+    #print(json_obj)
+#pull_helium_activity("112ubhsPKt3jN5iHmzjtv7Qma4N9KTdbUdw1DpJF1LHfKy7SbFRc","01/01/22")
+
