7e55c8187f708ce18785b3bb4050692b616c805b
NMcVayZag <Nickwmcvay15@gmail.com>
2022-05-04 12:34:52 -0700

    update

--- Utils2.py
+++ Utils2.py
@@ -12,3 +12,14 @@ def weekend_filter(df):
     df["Weekend"] = adj
     return(df)
 
+def scaler(df):    
+    from sklearn.preprocessing import MinMaxScaler
+    import numpy as np
+    a = list(df["hnt_Mined"].reset_index(drop=True))
+    b = list(df["tmax"].reset_index(drop=True))
+    scaler = MinMaxScaler()
+    mining_shaped = np.reshape(a,(-1,1))
+    tempature_shaped = np.reshape(b,(-1,1))
+    mining_scaled= scaler.fit_transform(mining_shaped)
+    tempature_scaled = scaler.fit_transform(tempature_shaped)
+    return mining_scaled, tempature_scaled
