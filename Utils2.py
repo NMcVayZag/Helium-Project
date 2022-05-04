40e0ee5f23446083a7469340c5d04e9863cb0f36
NMcVayZag <Nickwmcvay15@gmail.com>
2022-05-04 12:27:34 -0700

    update

--- Utils2.py
+++ Utils2.py
@@ -1,13 +1,14 @@
-import requests
-import json
 import pandas as pd
 import numpy as np
-#def pull_helium_activity(helium_address,min_time):
-    #url = "https://api.helium.io/v1/hotspots/:address/activity"
-    #url += "?address="+ helium_address + "&min_time=" + "1593622131"
-    #response = requests.get(url=url)
-    #json_str = response.text        #takes response turning it to a string
-    #json_obj = json.loads(json_str)
-    #print(json_obj)
-#pull_helium_activity("112ubhsPKt3jN5iHmzjtv7Qma4N9KTdbUdw1DpJF1LHfKy7SbFRc","01/01/22")
+def weekend_filter(df):
+    adj = []
+    for i in df["Weekend"]:
+        if i == 1:
+            adj.append(int(1))
+        elif i==7:
+            adj.append(int(1))
+        else:
+            adj.append(int(0)) 
+    df["Weekend"] = adj
+    return(df)
 
